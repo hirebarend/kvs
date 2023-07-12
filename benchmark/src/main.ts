248218ee9ec72a6114cb790ecd5cf7d251a46016
Barend Erasmus <hirebarend@gmail.com>
2023-07-12 15:45:47 +0200

    update

--- benchmark/src/main.ts
+++ benchmark/src/main.ts
@@ -13,19 +13,21 @@ function run() {
     const socket: net.Socket = new net.Socket();
 
     socket.connect(1337, process.env.HOST || '20.87.234.215', () => {
+      console.log(process.env.HOST || '20.87.234.215');
+
       const timestamp1 = new Date().getTime();
 
       let n = 0;
 
       socket.on('data', (data: Buffer) => {
-        console.log(data);
+        // console.log(data);
 
-        if (n >= 1_0_000) {
+        if (n >= 1_000) {
           const timestamp2 = new Date().getTime();
 
           console.log(timestamp2 - timestamp1);
           console.log((timestamp2 - timestamp1) / 1000);
-          console.log(1_0_000 / ((timestamp2 - timestamp1) / 1000));
+          console.log(1_000 / ((timestamp2 - timestamp1) / 1000));
           console.log('-----------------------------------------');
 
           socket.destroy();
